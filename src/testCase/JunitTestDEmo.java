cebe394dcbaf901de0862668dd0aa998b7c9be1b
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2021-09-24 11:40:24 +0530

    test case change

--- src/testCase/JunitTestDEmo.java
+++ src/testCase/JunitTestDEmo.java
@@ -21,20 +21,28 @@ class JunitTestDEmo {
 	ReceiptService receiptService = new ReceiptService();
 	 
 	@Test
-	void testTotalInvoice() {
+	void testTotalInvoice1() {
 		List<Product> productList = getProductList1();
 		receiptService.calculateReceipt(productList);
 		assertEquals(getPriceOfEachProductInList1(), productList.stream().map(Product::getTotalPrice).collect(Collectors.toList()));
-        assertEquals(BigDecimal.valueOf(330.83), receiptService.getFinalAmount());
+        assertEquals(BigDecimal.valueOf(330.83).setScale(2), receiptService.getFinalAmount());
         assertEquals(BigDecimal.valueOf(15.00).setScale(2), receiptService.getTotalTax());
 	}
 
+	@Test
+	void testTotalInvoice2() {
+		List<Product> productList = getProductList2();
+		receiptService.calculateReceipt(productList);
+		assertEquals(getPriceOfEachProductInList2(), productList.stream().map(Product::getTotalPrice).collect(Collectors.toList()));
+        assertEquals(BigDecimal.valueOf(646.10).setScale(2), receiptService.getFinalAmount());
+        assertEquals(BigDecimal.valueOf(75.60).setScale(2), receiptService.getTotalTax());
+	}
+	
 	private List<Product> getProductList1() {
 		 List<Product> productList1 = new ArrayList<Product>();
 	        productList1.add(new Product("book", 1, new BigDecimal("124.99"), new BigDecimal("10"), new BigDecimal("5"), false, true));
 	        productList1.add(new Product("music CD", 1, new BigDecimal("149.99"), new BigDecimal("10"), new BigDecimal("5"), false, false));
 	        productList1.add(new Product("chocolate bar", 1, new BigDecimal("40.85"), new BigDecimal("10"), new BigDecimal("5"), false, true));
-
 		return productList1;
 	}
 	
@@ -45,5 +53,20 @@ class JunitTestDEmo {
         expectedPriceOfEachProductInList1.add(new BigDecimal("40.85"));
 		return expectedPriceOfEachProductInList1;
 	}
+	
+	private List<Product> getProductList2(){
+        List<Product> productList2 = new ArrayList<>();
+        productList2.add(new Product("imported chocolates", 1, new BigDecimal("100.00"), new BigDecimal("10"), new BigDecimal("5"), true, true));
+        productList2.add(new Product("imported perfume", 1, new BigDecimal("470.50"), new BigDecimal("10"), new BigDecimal("5"), true, false));
+		return productList2;
+	}
+	
+	private List<BigDecimal> getPriceOfEachProductInList2(){
+		List<BigDecimal> expectedPriceOfEachProductInList2 = new ArrayList<>();
+        expectedPriceOfEachProductInList2.add(new BigDecimal("105.00"));
+        expectedPriceOfEachProductInList2.add(new BigDecimal("541.10"));
+		return expectedPriceOfEachProductInList2;
+	}
+	
 
 }
